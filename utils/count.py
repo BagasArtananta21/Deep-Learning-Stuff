80958b6e98af5b14740df1c168bd9034935b7e96
bagas <672023163@student.uksw.edu>
2025-11-21 01:52:57 +0700

    main

--- utils/count.py
+++ utils/count.py
@@ -6,13 +6,9 @@ def update_video_class_count(results_frame, class_names):
     """
     frame_count = {}
     
-    # results_frame.boxes berisi semua kotak yang terdeteksi di frame ini
     for box in results_frame.boxes:
         cls_id = int(box.cls[0])
-        # Ambil nama label dari class_names (best.pt)
         label = class_names.get(cls_id, f"class_{cls_id}")
-        
-        # Tambah hitungan
         frame_count[label] = frame_count.get(label, 0) + 1
 
     return frame_count
